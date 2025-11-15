e69d4e93123e11393ce1f0d60391ec95bfa7d223
ProgecktProf <sergey.nikishin555123123@gmail.com>
2025-11-15 22:17:17 +0300

    Create App.tsx

---
+++ apps/webapp/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Академия АНБ</h1>
+        <p>Добро пожаловать в мини-приложение!</p>
+        <div className="loading">
+          Приложение загружается...
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
